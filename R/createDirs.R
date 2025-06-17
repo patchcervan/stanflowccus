42d324db2a6f43d6011662f9c4301cce3c2be563
patchcervan <francerper@gmail.com>
2025-06-17 10:02:42 +0200

    CreateDirs have a verbose option

--- R/createDirs.R
+++ R/createDirs.R
@@ -3,6 +3,7 @@
 #' This function creates the directory structure for an occupancy modelling workflow
 #'
 #' @param project_dir Character. The path where the structure should be created. Defaults to the current working directory.
+#' @param verbose Logical. If TRUE (default) directories created and omitted are printed.
 #'
 #' @details
 #' This function is meant to be run at an empty directory to start working on a workflow.
@@ -18,13 +19,13 @@
 #' }
 #'
 #' @export
-createDirs <- function(project_dir = getwd()){
+createDirs <- function(project_dir = getwd(), verbose = TRUE){
 
     dirs <- file.path(project_dir, c("data", "output", "models", "scripts"))
 
     dirs_create <- dirs[!dir.exists(dirs)]
 
-    if(any(dir.exists(dirs))){
+    if(verbose & any(dir.exists(dirs))){
         warning(paste0("Couldn't create directories: ",
                       paste(dirs[dir.exists(dirs)], collapse = ", "),
                       ", because they already exist"))
@@ -34,6 +35,8 @@ createDirs <- function(project_dir = getwd()){
         dir.create(i)
     }
 
-    message(paste0("Directories created: ", paste(dirs_create, collapse = ", ")))
+    if(verbose){
+        message(paste0("Directories created: ", paste(dirs_create, collapse = ", ")))
+    }
 
 }
